0ad24f3e04b550761fac09c1f6acd13e9d38ff05
Chr_ <chr@chrxw.com>
2023-01-29 17:59:50 +0800

    misc 小更新

--- app/src/main/java/com/chrxw/purenga/MainActivity.kt
+++ app/src/main/java/com/chrxw/purenga/MainActivity.kt
@@ -1,9 +1,11 @@
 package com.chrxw.purenga
 
 import android.content.ComponentName
+import android.content.Context
 import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.preference.Preference
 import androidx.preference.PreferenceFragmentCompat
