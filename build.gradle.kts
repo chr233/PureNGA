f82c49da25e793564dc2823c5fbe1d94f39cf62c
chr_ <chr@chrxw.com>
2024-10-30 14:04:28 +0800

    update

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-    id("com.android.library") version "8.7.0" apply false
-    id("com.android.application") version "8.7.0" apply false
+    id("com.android.library") version "8.7.1" apply false
+    id("com.android.application") version "8.7.1" apply false
     id("org.jetbrains.kotlin.android") version "1.9.0" apply false
 }
