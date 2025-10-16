173ebd59133d3b618be4ddcd624fbc0f4a996a97
chr_ <chr@chrxw.com>
2025-10-16 16:49:46 +0800

    misc

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,6 +1,22 @@
 pluginManagement {
     repositories {
+        maven {
+            name = "google mirror"
+            url = uri("https://maven.aliyun.com/repository/google")
+        }
+        maven {
+            name = "central mirror"
+            url = uri("https://maven.aliyun.com/repository/central")
+        }
         google()
+        maven {
+            name = "Xposed"
+            url = uri("https://api.xposed.info/")
+        }
+        maven {
+            name = "Sonatype Releases"
+            url = uri("https://s01.oss.sonatype.org/content/repositories/releases")
+        }
         mavenCentral()
         gradlePluginPortal()
     }
@@ -11,6 +27,14 @@ plugins {
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
+        maven {
+            name = "google mirror"
+            url = uri("https://maven.aliyun.com/repository/google")
+        }
+        maven {
+            name = "central mirror"
+            url = uri("https://maven.aliyun.com/repository/central")
+        }
         google()
         maven {
             name = "Xposed"
